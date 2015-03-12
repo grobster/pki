0c2cf1bcf7807d0033ec173cce056947f19216a7
grobster <jquarles@grobster.com>
2015-03-12 08:59:11 -0400

    added arraylists to ControlPanel class.

--- javapki/source/com/grobster/pki/ControlPanel.java
+++ javapki/source/com/grobster/pki/ControlPanel.java
@@ -5,4 +5,8 @@ import java.util.*;
 
 public class ControlPanel implements Serializable{
 	private ArrayList<Request> pendingRequests;
+	private ArrayList<Request> vendorRequests;
+	private ArrayList<Request> completedRequests;
+	private ArrayList<Request> archiveRequests;
+	private ArrayList<Request> errorsRequests;
 }
