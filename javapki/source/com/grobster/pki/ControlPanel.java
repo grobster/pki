b745d9651542052dfff3c82077441fdc4d940180
grobster <jquarles@grobster.com>
2015-03-12 09:08:37 -0400

    added move methods to ControlPanel class.

--- javapki/source/com/grobster/pki/ControlPanel.java
+++ javapki/source/com/grobster/pki/ControlPanel.java
@@ -4,9 +4,38 @@ import java.io.*;
 import java.util.*;
 
 public class ControlPanel implements Serializable{
+	private static final long serialVersionUID = 357277532942L;
 	private ArrayList<Request> pendingRequests;
 	private ArrayList<Request> vendorRequests;
 	private ArrayList<Request> completedRequests;
 	private ArrayList<Request> archiveRequests;
 	private ArrayList<Request> errorsRequests;
+	
+	public ControlPanel() {
+		pendingRequests = new ArrayList<>();
+		vendorRequests = new ArrayList<>();
+		completedRequests = new ArrayList<>();
+		archiveRequests = new ArrayList<>();
+		errorsRequests = new ArrayList<>();
+	}
+	
+	public void moveToPendingRequests(Request r) {
+		pendingRequests.add(r);
+	}
+	
+	public void moveToVendorRequests(Request r) {
+		vendorRequests.add(r);
+	}
+	
+	public void moveToCompletedRequests(Request r) {
+		completedRequests.add(r);
+	}
+	
+	public void moveToArchiveRequests(Request r) {
+		archiveRequests.add(r);
+	}
+	
+	public void moveToerrorsRequests(Request r) {
+		errorsRequests.add(r);
+	}
 }
